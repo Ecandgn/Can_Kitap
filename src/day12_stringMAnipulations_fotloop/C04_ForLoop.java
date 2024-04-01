e216a953d0bf7f035067721b41fc0cfccbff76a9
Ecandgn <160060211+Ecandgn@users.noreply.github.com>
2024-04-01 20:50:46 +0300

    C04_ForLoop

--- src/day12_stringMAnipulations_fotloop/C04_ForLoop.java
+++ src/day12_stringMAnipulations_fotloop/C04_ForLoop.java
@@ -3,10 +3,22 @@ package day12_stringMAnipulations_fotloop;
 public class C04_ForLoop {
     public static void main(String[] args) {
         /*
-        1 den 100 e kadar olan sayıları yazdırın.
+        34 den başlayop 3 er 3er artarak 100 e kadar sayıları yazdırın.
         */
-        for (int i = 1; i <=100 ; i++) {
-            System.out.println(i);
+        for (int i = 34; i <= 100; i += 3) {
+            System.out.print(i+ " ");
+        }
+        System.out.println();
+
+        /*
+        iki basamaklı sayılardan 7 ile bölünebilenleri yazdırın
+         */
+
+        for (int i = 10; i < 100; i++) {
+            if (i % 7 == 0) {
+                System.out.print(i + " ");
+            }
+
         }
     }
 }
