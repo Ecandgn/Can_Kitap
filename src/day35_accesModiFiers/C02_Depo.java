081eaef005955917f94dd86677e6453a2ff18be9
Ecandgn <160060211+Ecandgn@users.noreply.github.com>
2024-04-08 22:10:50 +0300

    C02_Depo

---
+++ src/day35_accesModiFiers/C02_Depo.java
@@ -0,0 +1,16 @@
+package day35_accesModiFiers;
+
+public class C02_Depo {
+    private int privateSayi;
+    private static String privateStr;
+    int defaultSayi;
+    static String defaultStr;
+    protected intprotectedSayi;
+
+    protected static String protectedStr;
+
+    public int publicSayi;
+
+    public static void main(String[] args) {
+
+    }
