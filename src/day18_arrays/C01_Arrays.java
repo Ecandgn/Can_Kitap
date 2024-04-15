a5d503ba572649577a58b3cd0fa036229aaed4ac
Ecandgn <160060211+Ecandgn@users.noreply.github.com>
2024-04-15 19:56:50 +0300

    C01_Arrays

---
+++ src/day18_arrays/C01_Arrays.java
@@ -0,0 +1,14 @@
+package day18_arrays;
+
+public class C01_Arrays {
+    public static void main(String[] args) {
+        /* bir array iki şekilde oluşturabiliriz
+        1- deklarasyon ve içindeki tüm elementler yazılabilir
+         */
+        String[] isimler = {"Mahmut", "Hasan", "Ilgar","Sumeyra"};
+                String harfler[]={"a","l","d"};
+
+       // 2- deklerasyon ve array'ın eleman sayısı yazılıp array oluşturulabilir
+
+        }
+    }
