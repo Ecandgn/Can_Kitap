091310206cef2dff1c6e7e6198756ff51401a080
Ecandgn <160060211+Ecandgn@users.noreply.github.com>
2024-04-16 22:50:01 +0300

    C05_BaskaClassdanKullanma

--- src/day17_scope/C05_BaskaClassdanKullanma.java
+++ src/day17_scope/C05_BaskaClassdanKullanma.java
@@ -5,17 +5,18 @@ public class C05_BaskaClassdanKullanma {
         /*
         Methodlar ve class level variable'lar
         başka class'lardan da kullanılabilir
-        Static variable ve methodlara başka class dan oluşmak için classIsmi.classUyesiIsmi kullanılır
+        Static variable ve methodlara başka class dan uluşmak için
+        classIsmi.classUyesiIsmi kullanılır
         bu şekilde kullanıma static yontem denir
         Instance variablerları başka class dan kullanmak için
         Instance variable in olduğu class dan bir obje oluşturulmalıdır.
 
          */
         System.out.println(C04_StaticVariablesVsInstanceVariables.bashekimIsmi);
-        C04_StaticVariablesVsInstanceVariables personel=new C04_StaticVariablesVsInstanceVariables();
+        C04_StaticVariablesVsInstanceVariables personel=new C04_StaticVariablesVsInstanceVariables(); //Instance variable in olduğu class dan bir obje oluşturulmalıdır.
         System.out.println(personel.perAdresi);
         System.out.println(personel.bashekimIsmi); //static bir üyeye obje üzerinden ulaşmak isterseniz java otomatik olarak getirmez monuel olarak yazabiliriz.
 
 
-    }
+     }
 }
