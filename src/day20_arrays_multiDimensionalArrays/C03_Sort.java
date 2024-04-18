579c466349d3ce18ba77dcdf15331d955cb18b0f
Ecandgn <160060211+Ecandgn@users.noreply.github.com>
2024-04-18 22:00:38 +0300

    C04_BinarySearch

--- src/day20_arrays_multiDimensionalArrays/C03_Sort.java
+++ src/day20_arrays_multiDimensionalArrays/C03_Sort.java
@@ -1,5 +1,7 @@
 package day20_arrays_multiDimensionalArrays;
 
+import day19_arrays.C05_ElemanArama;
+
 import java.util.Arrays;
 
 public class C03_Sort {
@@ -11,5 +13,7 @@ int[] sayilar={2,6,10,9,52,4,23,1,4};
     String[] isimler={"Meltem", "Seren", "Ali", "Ahmet", "semih","Merve","Zam","Veli"}; //Harflere sırası ile bakarak sıralama yapar
     Arrays.sort(isimler);
         System.out.println(Arrays.toString(isimler));
+
+        C05_ElemanArama.elemanVarmi(sayilar,2); // aranan elmeanı başkaclasdan çağırma
     }
 }
