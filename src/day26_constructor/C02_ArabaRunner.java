d32a714e1f81012f4c7383286b8b9b5daf1615af
Ecandgn <160060211+Ecandgn@users.noreply.github.com>
2024-04-24 23:53:29 +0300

    C03_ArabaRunner

--- src/day26_constructor/C02_ArabaRunner.java
+++ src/day26_constructor/C02_ArabaRunner.java
@@ -3,7 +3,7 @@ package day26_constructor;
 public class C02_ArabaRunner {
     public static void main(String[] args) {
 
-        C01_Araba araba1=new C01_Araba();
+        C01_Araba araba1 = new C01_Araba();
         System.out.println(araba1);
 
 
@@ -13,10 +13,16 @@ public class C02_ArabaRunner {
         yil=1900,
          fiyat=0
          */
-   C01_Araba araba2;
+        C01_Araba araba2;
 
 //        System.out.println(araba2.fiyat);
+        // bir constructor çalışmadan objeye ilk değer ataması yapılmadan da kullanılamaz
 
-        System.out.println(new C01_Araba().fiyat);
+        System.out.println(new C01_Araba().fiyat); //0
+        // esitligin sadece sag tarafi olursa
+        // Constructor calistigi icin
+        // class'a gidip yeni bir obje olusturup, bize getirir
+        // ANCAK getirdigi objeyi kaydetmedigimiz icin
+        // O Obje sadece o satir icin VAR OLUR
     }
 }
--- src/day26_constructor/C03_ArabaRunner.java
+++ src/day26_constructor/C03_ArabaRunner.java
@@ -9,12 +9,13 @@ public class C03_ArabaRunner {
         araba1.model = "5.20";
         araba1.yil = 2001;
         araba1.fiyat = 4000;
-        System.out.println(araba1);
+        System.out.println(araba1); //Araba özellikler{marka='BMW', model='5.20', yil=2001, fiyat=4000}
+
         C01_Araba araba2 = new C01_Araba("Tofas", "Şahin", 1990, 15000);
         System.out.println(araba2);
         C01_Araba araba3 = new C01_Araba("Nissan", "note", 2020, 2000);
         System.out.println(araba3);
-        C01_Araba araba4=new C01_Araba("opel","corsa",2010,4500);
+        C01_Araba araba4 = new C01_Araba("opel", "corsa", 2010, 4500);
         System.out.println(araba4);
 
     }
