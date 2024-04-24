81e0da1d3664a289e1abbf4daa9c8ec86e555054
Ecandgn <160060211+Ecandgn@users.noreply.github.com>
2024-04-24 21:25:52 +0300

    C04_CarRunner

---
+++ src/day25_constructor/C03_Car.java
@@ -0,0 +1,19 @@
+package day25_constructor;
+
+public class C03_Car {
+    String marka = "Marka belirtilmedi";
+    String model = "Model belirtilmedi";
+    int yil = 1900;
+    int fiyat;
+
+    C03_Car() { // Constructor
+        marka = "Audi";
+        model = "A3";
+    }
+
+    int C03_Car() {   // Methoddur
+
+        return 0;
+    }
+
+}
