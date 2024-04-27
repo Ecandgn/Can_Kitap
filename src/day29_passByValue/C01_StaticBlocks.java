7599862dab11bb30705e155ec81690c8ee91000d
Ecandgn <160060211+Ecandgn@users.noreply.github.com>
2024-04-27 22:14:47 +0300

    C02_StaticOlmayanBlok

--- src/day29_passByValue/C01_StaticBlocks.java
+++ src/day29_passByValue/C01_StaticBlocks.java
@@ -1,7 +1,15 @@
 package day29_passByValue;
 
 public class C01_StaticBlocks {
+    /*
+    static bloklar tüm methodlar ve kod bloklarıından önce çalışır
+    (Main method dan bile önce çalışır)
+    class çalışmaya başlamadan yapılması gereken
+     herhangi bir işlem varsa static bloc'lar kullanılabilinir.
+     - eğer birden fazla static blok varsa üstteki daha önce çalışır
+     */
     C01_StaticBlocks(){
+
         System.out.println("Constructor calisti");
     }
 
@@ -10,10 +18,13 @@ public class C01_StaticBlocks {
         C01_StaticBlocks obj=new C01_StaticBlocks();
     }
     public static void method1(){
-        System.out.println("method çalıştı");
+        System.out.println("method1 çalıştı");
 
     }
     static {
         System.out.println("Static bloc çalıştı");
+    }
+    static {
+        System.out.println("Static blok1 çalıştı");
     }
 }
--- src/day29_passByValue/C02_StaticOlmayanBlok.java
+++ src/day29_passByValue/C02_StaticOlmayanBlok.java
@@ -1,19 +1,29 @@
 package day29_passByValue;
 
 public class C02_StaticOlmayanBlok {
-    C02_StaticOlmayanBlok(){
+    /*
+    static olmayan bloklar constructor dan önce çalışırı
+    ve (varsa) obje oluşturulmadan önce yapılması gereken ayarları yapar
+     */
+    C02_StaticOlmayanBlok() {
         System.out.println("Constructor Çalıştı");
+    }
+
+    C02_StaticOlmayanBlok(int sayi) {
+        System.out.println("Parametreli constructor çalıştı");
     }
 
     public static void main(String[] args) {
         System.out.println("Main method çalıştı");
 
-        C02_StaticOlmayanBlok obj1= new C02_StaticOlmayanBlok();
-        System.out.println("obj1 çalıştı");
+        C02_StaticOlmayanBlok obj1 = new C02_StaticOlmayanBlok();
+        System.out.println("obj1 oluşturuldu");
 
-        C02_StaticOlmayanBlok obj2=new C02_StaticOlmayanBlok();
+
+        C02_StaticOlmayanBlok obj2 = new C02_StaticOlmayanBlok(5);
         System.out.println("obj2 oluşturuldu");
     }
+
     {
         System.out.println("static olmayan blok çalıştı");
     }
