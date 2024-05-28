c5d4889dc1ed4851141fa519d9c1fb6f05517e3b
Ecandgn <160060211+Ecandgn@users.noreply.github.com>
2024-05-29 00:24:13 +0300

    C07_EntryKullanimi

---
+++ src/day48_OgrenciNumarasinaGoreListeYazdir/C04_BolumUpdate.java
@@ -0,0 +1,25 @@
+package day48_OgrenciNumarasinaGoreListeYazdir;
+
+import day47_maps.OgrenciMapClass;
+
+public class C04_BolumUpdate extends OgrenciMapClass {
+
+    public static void main(String[] args) {
+        // istenen bolumdeki tum ogrencilerin,
+        // bolum isimlerini yeni verilen bolum ismi olarak degistirin
+        // Ornek; MF bolumundeki tum ogrencilerin bolumunu SAY yapin
+
+        mapeOgrenciEkle();
+
+        System.out.println(ogrenciMap);
+
+        bolumDegistir("MF", "SAY");
+
+        System.out.println(ogrenciMap);
+
+        bolumDegistir("TM", "EA");
+
+        System.out.println(ogrenciMap);
+
+    }
+}
